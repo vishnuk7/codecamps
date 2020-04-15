5d7ed513b79f3615dea4f33a6b2f4299be113639
viu <vishnu.kraman88@gmail.com>
2020-04-15 14:02:19 +0530

    handling correctly formated id issue

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -23,6 +23,11 @@ exports.getCodecamps = async (req, res, next) => {
 exports.getCodecamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+      });
+    }
     res.status(200).json({
       success: true,
       data: bootcamp,
