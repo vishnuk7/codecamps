edceffb9b6d983935f003ebb5838ddf6bebcad9c
viu <vishnu.kraman88@gmail.com>
2020-04-17 14:42:24 +0530

    error handler middleware next(err)

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -34,9 +34,10 @@ exports.getCodecamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(400).json({
-      success: false,
-    });
+    // res.status(400).json({
+    //   success: false,
+    // });
+    next(err);
   }
 };
 
