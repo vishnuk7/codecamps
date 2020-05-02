881e798ab410f84db9dd5a4d1322e66ddd64a6dd
viu <vishnu.kraman88@gmail.com>
2020-05-03 00:54:27 +0530

    course controller

---
+++ controllers/coures.js
@@ -0,0 +1,26 @@
+const Course = require("../model/Coures");
+const asyncHandler = require("../middleware/asyncHandler");
+const ErrorResponese = require("../util/errorResponse");
+
+//@desc Get all codecamps
+//@route GET /api/v1/courses
+//@route GET /api/v1/codecamps/:codecampsId/course
+//@access Public
+
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query;
+
+  if (req.params.codecampsId) {
+    query = Course.find({ bootcamps: req.params.bootcamps });
+  } else {
+    query = Course.find();
+  }
+
+  const coures = await query;
+
+  res.status(200).json({
+    success: true,
+    count: Course.length,
+    data: course,
+  });
+});
