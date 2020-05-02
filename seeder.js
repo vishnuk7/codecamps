0c9f7c1d3d01730af9e5578dc1e71be4975e37b9
viu <vishnu.kraman88@gmail.com>
2020-05-02 22:43:34 +0530

    courses seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 
 //Load models
 const Bootcamp = require("./model/Bootcamp");
+const Course = require("./model/Coures");
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,10 +24,15 @@ const boocamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const course = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 // Importing into DB
 const importData = async () => {
   try {
     await Bootcamp.create(boocamps);
+    await Course.create(course);
     console.log(chalk`{green Data Imported ...}`);
     process.exit();
   } catch (err) {
@@ -38,6 +44,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log(chalk`{red Data deleted... }`);
     process.exit();
   } catch (err) {
