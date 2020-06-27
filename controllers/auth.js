6c2074add19c4a654e58b4272639f271652851e4
vsihnuk7 <vishnu.kraman88@gmail.com>
2020-06-27 18:54:51 +0530

    register user 👤

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,14 @@ const User = require("../model/User");
 //@route GET /api/v1/register
 //@access Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({ success: true });
 });
